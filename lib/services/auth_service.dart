2fb70cd9c0ec809f4f6025542d4bfee75d56b6f3
Sarath191181208 <vssarathc04@gmail.com>
2024-11-19 17:13:16 +0530

    login auth_service completed

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,3 @@
-import 'dart:developer';
-
-import 'package:mauthn_app/pages/register/replying_party_server.dart';
 import 'package:mauthn_app/services/api/api.dart';
 import 'package:mauthn_app/services/replying_party_server.dart';
 import 'package:passkeys/authenticator.dart';
@@ -11,21 +8,16 @@ class AuthService {
   final ReplyingPartyServer rps;
   final PasskeyAuthenticator authenticator;
 
-  /*
-  Future<void> loginWithPasskey({required String email}) async {
-    final rps1 = rps.startPasskeyLogin(name: email);
-    final authenticatorRes = await authenticator.authenticate(rps1);
-    rps.finishPasskeyLogin(response: authenticatorRes);
+  Future<void> loginWithPasskey(
+      {required ApiService apiHandler, required String userid}) async {
+    final rps1 =
+        await rps.startPasskeyLogin(apiHandler: apiHandler, userid: userid);
+    final authRes = await authenticator.authenticate(rps1);
+    rps.finishPasskeyLogin(
+        apiHandler: apiHandler, userid: userid, response: authRes);
   }
 
-  Future<void> loginWithPasskeyConditionalUI() async {
-    final rps1 = rps.startPasskeyLoginConditionalU();
-    final authenticatorRes = await authenticator.authenticate(rps1);
-    rps.finishPasskeyLoginConditionalUI(response: authenticatorRes);
-  }
-  */
-
-  Future<void> signupWithPasskey(
+  Future<String> signupWithPasskey(
       {required ApiService apiHandler, required String email}) async {
     final rps1 =
         await rps.startPasskeyRegister(apiHandler: apiHandler, name: email);
@@ -35,5 +27,6 @@ class AuthService {
       response: authenticatorRes,
       user: rps1.user,
     );
+    return rps1.user.id;
   }
 }
