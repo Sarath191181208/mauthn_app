a577c6b021bf9b3757c2277d7d5cbe8a28b8de36
Sarath191181208 <vssarathc04@gmail.com>
2024-11-19 14:55:46 +0530

    request op fix

--- lib/pages/register/replying_party_server.dart
+++ lib/pages/register/replying_party_server.dart
@@ -4,29 +4,36 @@ import 'package:mauthn_app/services/api/api.dart';
 import 'package:mauthn_app/services/api/auth.dart';
 import 'package:passkeys/types.dart';
 
-class LocalUser {
-  LocalUser({required this.name, required this.id, this.credentialID});
-
-  final String name;
-  final String id;
-  String? credentialID;
-}
-
 class ReplyingPartyServer {
   Future<RegisterRequestType> startPasskeyRegister(
       {required ApiService apiHandler, required String name}) async {
-    final data = jsonDecode((await fetchRegisterOptions(apiHandler, name)).data);
+    final data =
+        jsonDecode((await fetchRegisterOptions(apiHandler, name)).data);
     final rp = RelyingPartyType.fromJson(data["rp"]);
     final user = UserType.fromJson(data["user"]);
-    final authType = AuthenticatorSelectionType.fromJson(data["authenticatorSelection"]);
-    final req =  RegisterRequestType(
+    final authType =
+        AuthenticatorSelectionType.fromJson(data["authenticatorSelection"]);
+    final pubKeyCredParams = [
+      for (var item in data["pubKeyCredParams"])
+        PubKeyCredParamType.fromJson(item)
+    ];
+    final req = RegisterRequestType(
       challenge: data["challenge"],
       relyingParty: rp,
       user: user,
       authSelectionType: authType,
+      pubKeyCredParams: pubKeyCredParams,
       excludeCredentials: [],
     );
     return req;
   }
-}
 
+  Future<bool> finishPasskeyRegister(
+      {required ApiService apiHandler,
+      required RegisterResponseType response,
+      required UserType user}) async {
+    final res = await completeRegister(apiHandler, response, user);
+    const httpCreated = 201;
+    return res.statusCode == httpCreated;
+  }
+}
