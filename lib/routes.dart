fb4a0a7d087bbfa24cf80d3498843110abdad0bb
Sarath191181208 <vssarathc04@gmail.com>
2024-11-21 16:46:37 +0530

    default to signup page

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,15 +2,19 @@ import 'package:go_router/go_router.dart';
 import 'package:mauthn_app/pages/home/home.dart';
 import 'package:mauthn_app/pages/login/login.dart';
 import 'package:mauthn_app/pages/register/register.dart';
+import 'package:mauthn_app/pages/settings/settings.dart';
+import 'package:mauthn_app/pages/test/test.dart';
 
 class Routes {
   static const register = '/register';
   static const logIn = '/log-in';
   static const home = '/home';
+  static const settings = '/settings';
+  static const test = '/test';
 }
 
 final GoRouter router = GoRouter(
-  initialLocation: Routes.logIn,
+  initialLocation: Routes.register,
   routes: [
     GoRoute(
       path: Routes.register,
@@ -23,6 +27,10 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: Routes.home,
       builder: (context, state) => const HomePage(),
-    )
+    ),
+    GoRoute(
+      path: Routes.settings,
+      builder: (context, state) => const SettingsPage(),
+    ),
   ],
 );
