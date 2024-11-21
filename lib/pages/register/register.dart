a7f795cfc496c08bf58dcfb6a4e93205bce117ba
Sarath191181208 <vssarathc04@gmail.com>
2024-11-21 12:19:53 +0530

    register page redesign

--- lib/pages/register/register.dart
+++ lib/pages/register/register.dart
@@ -1,17 +1,48 @@
 import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mauthn_app/pages/basepage.dart';
 import 'package:mauthn_app/providers.dart';
 import 'package:mauthn_app/routes.dart';
 import 'package:mauthn_app/services/api/api.dart';
 
 import 'package:go_router/go_router.dart';
+import 'package:mauthn_app/services/auth_service.dart';
 
 final errorProvider = StateProvider<String?>((ref) => null);
 
 class RegisterPage extends ConsumerWidget {
   const RegisterPage({super.key});
+
+  Future<void> register(String email, ApiService apiHandler,
+      AuthService authService, WidgetRef ref, BuildContext context) async {
+    email = email.trim();
+    if (email.isEmpty) {
+      ref.read(errorProvider.notifier).state = 'Email address cannot be empty.';
+      return;
+    }
+
+    try {
+      final userId = await authService.signupWithPasskey(
+        apiHandler: apiHandler,
+        email: email,
+      );
+      ref.read(errorProvider.notifier).state = null;
+      ref.read(userIdProvider.notifier).state = userId;
+      context.go(Routes.logIn);
+      log("Signup successful for $email");
+    } catch (e) {
+      if (kDebugMode) {
+        ref.read(errorProvider.notifier).state = e.toString();
+      } else {
+        ref.read(errorProvider.notifier).state =
+            'An error occurred. Please try again.';
+      }
+      log("ERROR: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -20,118 +51,161 @@ class RegisterPage extends ConsumerWidget {
     final apiHandler = ApiService();
     final error = ref.watch(errorProvider);
 
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.fingerprint_rounded,
-                    size: 80,
-                    color: Colors.blue,
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    'Tired of passwords?',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    'Sign up using your biometrics like fingerprint or face.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  const SizedBox(height: 40),
-                  TextField(
-                    controller: emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.email_rounded),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12)),
-                      ),
-                      hintText: 'Email address',
-                    ),
-                  ),
-                  if (error != null)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Text(
-                        error,
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.error,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton.icon(
-                      onPressed: () async {
-                        final email = emailController.value.text.trim();
-                        if (email.isEmpty) {
-                          ref.read(errorProvider.notifier).state =
-                              'Email address cannot be empty.';
-                          return;
-                        }
-
-                        try {
-                          final userId = await authService.signupWithPasskey(
-                            apiHandler: apiHandler,
-                            email: email,
-                          );
-                          ref.read(errorProvider.notifier).state =
-                              null; // Clear error
-                          ref.read(userIdProvider.notifier).state = userId;
-                          context.go(Routes.logIn);
-                          log("Signup successful for $email");
-                        } catch (e) {
-                          ref.read(errorProvider.notifier).state = e.toString();
-                          log("ERROR: $e");
-                        }
-                      },
-                      icon: const Icon(Icons.lock_open_rounded),
-                      label: const Text(
-                        'Sign Up',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  TextButton.icon(
-                    onPressed: () {
-                      // Navigate to login or other actions
-                      context.go(Routes.logIn);
-                      log("Navigate to login");
-                    },
-                    icon: const Icon(Icons.login_rounded),
-                    label: const Text('Already have an account? Log in'),
-                  ),
-                ],
+    return BasePage(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: 128),
+          const Icon(
+            Icons.fingerprint,
+            size: 80,
+            color: Colors.white,
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "Tired of passwords?",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            "Sign up using your biometrics like fingerprint or face.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.8),
+            ),
+          ),
+          if (error != null)
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text(
+                error,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.error,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          const SizedBox(height: 32),
+          TextField(
+            controller: emailController,
+            style: const TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.2),
+              hintText: "Enter your email",
+              hintStyle: const TextStyle(color: Colors.white70),
+              prefixIcon: const Icon(Icons.mail, color: Colors.white),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+              contentPadding: const EdgeInsets.symmetric(vertical: 16),
+            ),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton.icon(
+            onPressed: () => register(
+              emailController.text,
+              apiHandler,
+              authService,
+              ref,
+              context,
+            ),
+            icon: const Icon(Icons.fingerprint),
+            label: const Text("Sign Up with Biometrics"),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              minimumSize: const Size.fromHeight(50),
+              padding: const EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 24,
+              ),
+              textStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
               ),
             ),
           ),
-        ),
+          TextButton.icon(
+            onPressed: () {
+              // Navigate to login or other actions
+              context.go(Routes.logIn);
+              log("Navigate to login");
+            },
+            icon: const Icon(Icons.login_rounded),
+            label: const Text('Already have an account? Log in'),
+          ),
+          const SizedBox(height: 32),
+          Text(
+            "Secure • Fast • Convenient",
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.8),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.smartphone, color: Colors.white, size: 24),
+              SizedBox(width: 16),
+              Icon(Icons.shield, color: Colors.white, size: 24),
+              SizedBox(width: 16),
+              Icon(Icons.lock, color: Colors.white, size: 24),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildIcon(IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Icon(
+        icon,
+        size: 32,
+        color: Colors.white,
       ),
     );
   }
 }
+
+/*
+                final email = emailController.value.text.trim();
+                if (email.isEmpty) {
+                  ref.read(errorProvider.notifier).state =
+                      'Email address cannot be empty.';
+                  return;
+                }
+
+                try {
+                  final userId = await authService.signupWithPasskey(
+                    apiHandler: apiHandler,
+                    email: email,
+                  );
+                  ref.read(errorProvider.notifier).state = null; // Clear error
+                  ref.read(userIdProvider.notifier).state = userId;
+                  context.go(Routes.logIn);
+                  log("Signup successful for $email");
+                } catch (e) {
+                  ref.read(errorProvider.notifier).state = e.toString();
+                  log("ERROR: $e");
+                }
+
+
+          TextButton.icon(
+            onPressed: () {
+              // Navigate to login or other actions
+              context.go(Routes.logIn);
+              log("Navigate to login");
+            },
+            icon: const Icon(Icons.login_rounded),
+            label: const Text('Already have an account? Log in'),
+          ),
+*/
